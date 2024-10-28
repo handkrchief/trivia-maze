f1b86d23ebd2de2c5ef118ed65d2b761798c8d2a
cabedev69 <caleb.carroll6@gmail.com>
2024-10-28 12:07:08 -0700

    flushed out some of the method descriptions.

--- src/model/Game.java
+++ src/model/Game.java
@@ -13,20 +13,39 @@ public class Game {
     
   private Maze myMaze;
   
-  //private Player myPlayer; //commented out until player class is created.
+  //private Player myPlayer; //commented out until player class is created to avoid error.
   
+  /**
+   * Responsible for handling the start of the game.
+   * Starting the game could include choosing the size of the maze to be completed.
+   */
   private void startGame() {
       //TO-DO
   }
   
+  /**
+   * Responsible for handling the end of the game.
+   * Saves the state of the game at its current stopping point.
+   */
   private void endGame() {
       //TO-DO
   }
   
+  /**
+   * Responsible for saving the state of the game to a file.
+   * Uses a JFileChooser or some other method of file choosing
+   * to direct the save files' location and name.
+   * Used to save the game at a point that the player desires.
+   */
   private void saveGame() {
       //TO-DO
   }
   
+  /**
+   * Loads a file that contains save state information.
+   * All data relevant to the state of the game, will be loaded
+   * for a continuation of the loaded saves session.
+   */
   private void loadGame() {
       //TO-DO
   }
