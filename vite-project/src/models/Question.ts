1ec3314b0e25af5d57d805c83dbfb871f0041796
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-09 11:40:25 -0800

    Update Question.ts with JavaDoc

--- vite-project/src/models/Question.ts
+++ vite-project/src/models/Question.ts
@@ -1,10 +1,33 @@
 import Item from "./Item";
 
+/**
+ * Question class
+ * 
+ * Represents a question in the game.
+ * 
+ * @author Zach Sanchez
+ * @version 1.0
+*/
+
 export default class Question{
+    /**
+     * Static string for incorrect text
+    */
     public static INCORRECT_TEXT = "INCORRECT_ANSWER";
 
+    /**
+     * The question text, with a "!" to signify that it will force initialization if not set
+     */
     private myQuestionText!: string;
+
+    /**
+     * The correct answer, with a "!" to signify that it will force initialization if not set
+     */
     private myCorrectAnswer!: string;
+
+    /**
+     * The choices for the question, with a "!" to signify that it will force initialization if not set
+     */
     private myChoices!: string[];
 
     constructor(theQuestionText: string, theCorrectAnswer: string, theChoices: string[]) {
@@ -13,6 +36,13 @@ export default class Question{
         this.myChoices = this.setMyChoices(theChoices);
     }
 
+
+    /**
+     * Applies an item to the question
+     * 
+     * @param {Item} theItem - The item to apply
+     * @returns {void}
+    */
     public applyItem(theItem: Item): void {
         let itemType = theItem.getItemType();
         if(itemType === "50-50"){
@@ -21,17 +51,27 @@ export default class Question{
             this.applyPhoneAFriend();
         }
     }
+
+    /**
+     * Returns a random wrong answer
+     * 
+     * @returns {string} - A random wrong answer
+    */
     private wrongAnswer(): string {
         let randomIndex = Math.floor(Math.random() * this.myChoices.length);
         let wrongAnswers = this.myChoices.filter(choice => choice !== this.myCorrectAnswer);
 
         return wrongAnswers[randomIndex];
     }
+
+    /**
+     * Applies the Phone-a-Friend item to the question
+     * There is a 10% chance that the friend will give the wrong answer
+     * 
+     * @returns {void}
+    */
     private applyPhoneAFriend(): void {
         let chance = Math.random();
-
-        
-       
         if(chance < 0.1) {
             alert( this.wrongAnswer());
         } else {
@@ -39,6 +79,12 @@ export default class Question{
         }
     }
 
+    /**
+     * Applies the Fifty-Fifty item to the question
+     * Eliminates two wrong answers
+     * 
+     * @returns {void}
+    */
     private applyFiftyFifty(): void {
         if(this.myChoices.includes(Question.INCORRECT_TEXT)){
             console.log("Fifty fifty can only be applied on the first turn.")
@@ -56,10 +102,21 @@ export default class Question{
         console.log("Fifty fifty eliminated 2 choices")
     }
 
+    /**
+     * Invalidates a choice
+     * 
+     * @param {number} theInvalidChoice - The index of the choice to invalidate
+     * @returns {void}
+    */
     private invalidateChoice(theInvalidChoice:number): void {
         this.myChoices[theInvalidChoice] = Question.INCORRECT_TEXT;
     }
 
+    /**
+     * Asks the question and returns whether the answer is correct
+     * 
+     * @returns {boolean} - Whether the answer is correct
+    */
     public askQuestion():boolean{
         this.printQuestion();
         this.printChoices();
@@ -80,18 +137,39 @@ export default class Question{
         return isCorrect;
     }
     
+    /**
+     * Gets the question text
+     * 
+     * @returns {string} - The question text
+    */
     public getMyQuestionText(): string {
         return this.myQuestionText;
     }
 
+    /**
+     * Gets the correct answer
+     * 
+     * @returns {string} - The correct answer
+    */
     public getMyCorrectAnswer(): string {
         return this.myCorrectAnswer;
     } 
 
+    /**
+     * Gets the choices for the question
+     * 
+     * @returns {string[]} - The choices for the question
+     */
     public getMyChoices(): string[] {
         return this.myChoices;
     }   
 
+    /**
+     * Sets the questions text
+     * 
+     * @param {string} theQuestionText - The text to set as the question text
+     * @returns {string} - The question text
+     */
     public setMyQuestionText(theQuestionText: string): string {
         if(theQuestionText.length === 0) {
             throw new Error(Question.INCORRECT_TEXT);
@@ -99,6 +177,12 @@ export default class Question{
        return theQuestionText;
     }
 
+    /**
+     * Sets the correct answer
+     * 
+     * @param {string} theCorrectAnswer - The correct answer to set
+     * @returns {string} - The correct answer
+     */
     public setMyCorrectAnswer(theCorrectAnswer: string): string {
         if(theCorrectAnswer.length === 0) {
             throw new Error(Question.INCORRECT_TEXT);
@@ -106,6 +190,13 @@ export default class Question{
         return theCorrectAnswer;
     }
 
+
+    /**
+     * Sets the choices for the question
+     * 
+     * @param {string[]} theChoices - The choices to set
+     * @returns {string[]} - The choices for the question
+     */
     public setMyChoices(theChoices: string[]): string[] {
         if(theChoices.length === 0) {
             throw new Error(Question.INCORRECT_TEXT);
@@ -113,10 +204,21 @@ export default class Question{
         return theChoices;
     }
 
+
+    /**
+     * Prints the question
+     * 
+     * @returns {void}
+    */
     private printQuestion(): void {
         console.log(this.myQuestionText);
     }
 
+    /**
+     * Prints the choices
+     * 
+     * @returns {void}
+    */
     private printChoices(): void {
         let choiceNumber = 1;
         this.myChoices.forEach(choice => {
@@ -125,6 +227,11 @@ export default class Question{
         });
     }
 
+    /**
+     * prints the result of the question
+     * 
+     * @param {boolean} result - Whether the answer is correct
+     */
     private printResult(result: boolean): void {
         if(result) {
             console.log("Correct!");
@@ -133,6 +240,11 @@ export default class Question{
         }
     }
     
+    /**
+     * Returns the question text as a string
+     * 
+     * @returns {string} - The question text
+     */
     public toString(): string {
         return this.myQuestionText;
     }    
