e190f9665e0ac63db3843493e80a9ea721846552
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-09 11:40:34 -0800

    Update Room.ts with JavaDoc

--- vite-project/src/models/Room.ts
+++ vite-project/src/models/Room.ts
@@ -1,16 +1,56 @@
 import Item from "./Item";
 import Question from "./Question";
 
+/**
+ * Room class
+ * 
+ * Represents a room in the game
+ * 
+ * @author Zach Sanchez
+ * @author Ethan Moore
+ * @version 1.0
+ */
 export default class Room {
+
+    /**
+     * The row of the room
+     */
     private myRow: number;
+    /**
+     * The column of the room
+     */
     private myCol: number;
+    /**
+     * Whether the room is open
+     */
     private isOpen: boolean;
+    /**
+     * Whether the room is locked
+     */
     private isLocked: boolean;
+    /**
+     * Whether the room is an item room
+     */
     private isItemRoom: boolean;
+    /**
+     * Whether the room is answered
+     */
     private isAnswered: boolean;
+    /**
+     * The question in the room
+     */
     private myQuestion?: Question;
+    /**
+     * The item in the room
+     */
     private myItem?: Item;
     
+    /**
+     * Constructor for the Room class
+     * 
+     * @param {number} theRow - The row of the room
+     * @param {number} theCol - The column of the room
+     */
     constructor({theRow, theCol}:{theRow:number, theCol:number}){
         this.myRow = theRow;
         this.myCol = theCol;
@@ -22,46 +62,95 @@ export default class Room {
         this.myItem = undefined;
     }
 
+    /**
+     * Gets the item in the room
+     * 
+     * @returns {Item} - The item in the room
+     */
     public getItem(){
         return this.myItem;
     }
     
+    /**
+     * Gets the question in the room
+     * 
+     * @returns {Question} - The question in the room
+     */
     public getQuestion(){
         return this.myQuestion;
     }
 
+    /**
+     * Gets the row of the room
+     * 
+     * @returns {number} - The row of the room
+     */
     public getRow(){
         return this.myRow;
     }
 
+    /**
+     * Gets the column of the room
+     * 
+     * @returns {number} - The column of the room
+     */
     public getCol(){
         return this.myCol;
     }
 
+    /**
+     * Gets whether the room is locked
+     * 
+     * @returns {boolean} - Whether the room is locked
+     */
     public getIsLocked(){
         return this.isLocked;
     }
 
+    /**
+     * Gets whether the room is open
+     * 
+     * @returns {boolean} - Whether the room is open
+     */
     public getIsOpen(){
         return this.isOpen;
     }
 
+    /**
+     * Gets whether the room is an item room
+     * 
+     * @returns {boolean} - Whether the room is an item room
+     */
     public getIsItemRoom(){
         return this.isItemRoom;
     }
 
-
-
+    /**
+     * Gets whether the room is answered
+     * 
+     * @returns {boolean} - Whether the room is answered
+     */
     public getIsAnswered(){
         return this.isAnswered;
     }
 
+    /**
+     * Sets the item in the room
+     * 
+     * @param {Item} item - The item to set
+     */
     public setItem(item:Item){
         if(item !== null){
             this.myItem = item;
         }
+        throw new Error("Item cannot be null");
     }
     
+    /**
+     * Sets the question in the room
+     * 
+     * @param {Question} question - The question to set
+     */
     public setQuestion(question:Question){
         if(question !== null){
             this.myQuestion = question;
@@ -73,38 +162,65 @@ export default class Room {
         if(row !== null){
             this.myRow = row;
         }
+        throw new Error("Row cannot be null");
     }
 
     public setCol(col:number){
         if(col !== null){
             this.myCol = col;
         }
+        throw new Error("Column cannot be null");
     }   
 
+    /**
+     * Sets whether the room is answered
+     * 
+     * @param {boolean} answeredStatus - Whether the room is answered
+     */
     public setIsAnswered(answeredStatus:boolean){
         if(answeredStatus !== null){
             this.isAnswered = answeredStatus;
         }
     }
 
+    /**
+     * Sets whether the room is an item room
+     * 
+     * @param {boolean} itemRoomStatus - Whether the room is an item room
+     */
     public setIsItemRoom(itemRoomStatus:boolean){
         if(itemRoomStatus !== null){
             this.isItemRoom = itemRoomStatus;
         }
     }
 
+    /**
+     * Sets whether the room is locked
+     * 
+     * @param {boolean} lockedStatus - Whether the room is locked
+     */
     public setIsLocked(lockedStatus:boolean){
         if(lockedStatus !== null){
             this.isLocked = lockedStatus;
         }
     }
 
+    /**
+     * Sets whether the room is open
+     * 
+     * @param {boolean} openStatus - Whether the room is open
+     */
     public setIsOpen(openStatus:boolean){
         if(openStatus !== null){
             this.isOpen = openStatus;
         }
     }
 
+    /**
+     * Checks if the room has an item
+     * 
+     * @returns {boolean} - Whether the room has an item
+     */
     public hasItem(){
         return this.myItem !== undefined;
     }
