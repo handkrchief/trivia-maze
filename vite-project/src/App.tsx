878c3d4e595cf52a50cdd237b656157c7ce9cd66
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-10 14:23:26 -0800

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from "react";
 import GameComponent from "./components/GameComponent/GameComponent"
 import HeaderBar from "./components/HeaderBar/HeaderBar"
 import { MazeContextProvider } from "./context/MazeContext"
-import s from './Home.module.css'
 import './index.css'
+import { ThemeContextProvider } from "./context/ThemeContext";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  } 
- 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [darkMode]);
   
   return (
-    <MazeContextProvider>
-      <div className={s.container}>
-        <HeaderBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
-      <GameComponent />
-      </div>
-    </MazeContextProvider>
+    <ThemeContextProvider>
+       <MazeContextProvider>
+        <Layout>
+          <HeaderBar />
+          <GameComponent />
+        </Layout>
+      </MazeContextProvider>
+    </ThemeContextProvider>
   )
 }
 
