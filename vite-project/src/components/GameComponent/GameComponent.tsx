8d6846533d6e2b73a8cd7fc15337c52d63888c08
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-13 23:53:30 -0800

    Update GameComponent.tsx

--- vite-project/src/components/GameComponent/GameComponent.tsx
+++ vite-project/src/components/GameComponent/GameComponent.tsx
@@ -3,15 +3,18 @@ import { useMazeContext } from '../../context/MazeContext';
 import PreGameOptionsComponent from '../PreGameOptionsComponent/PreGameOptionsComponent';
 import MazeComponent from '../MazeComponent/MazeComponent';
 import NavigationComponent from '../NavigationComponent/NavigationComponent';
+import TvComponent from '../TvComponent/TvComponent';
+import DeskComponent from '../DeskComponent/DeskComponent';
 
 export default function GameComponent() {
     const {myMaze,  started} = useMazeContext();
-
+    
   return (      
     <div className={`background ${s.container}`}>
         {!started && <div className={s.preGameOptionsContainer}><PreGameOptionsComponent /></div>}
 
-        {started && myMaze && <MazeComponent maze={myMaze.getMaze()} />}
+        {started && myMaze && <TvComponent />}
+        {started && myMaze && <DeskComponent/>}
         {started && <NavigationComponent />}
     </div>
   )
