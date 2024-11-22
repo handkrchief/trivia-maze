9c364c4b8fe214ba310a8a44d4102e627779565a
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-21 17:59:53 -0800

    Add BreakPoints to Tailwind config file

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -6,7 +6,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+      'xs':'200px',
+      'sm': '400px',
+      'md': '666px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1920px',
+      '3xl':'2000px',
+    },
+  },
   },
   plugins: [],
 }
