64f231690b78bf994b974f70b3fc4aaee36bcbce
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-21 18:01:41 -0800

    Update GameComponent.tsx and styling.

--- vite-project/src/components/GameComponent/GameComponent.tsx
+++ vite-project/src/components/GameComponent/GameComponent.tsx
@@ -1,11 +1,21 @@
 import s from './GameComponent.module.css'
 import { useMazeContext } from '../../context/MazeContext';
 import PreGameOptionsComponent from '../PreGameOptionsComponent/PreGameOptionsComponent';
-import MazeComponent from '../MazeComponent/MazeComponent';
 import NavigationComponent from '../NavigationComponent/NavigationComponent';
 import TvComponent from '../TvComponent/TvComponent';
 import DeskComponent from '../DeskComponent/DeskComponent';
 import QuestionAnswerComponent from '../QuestionAnswerComponent/QuestionAnswerComponent';
+
+/**
+ * The GameComponent is the main component that renders the game.
+ * It renders the PreGameOptionsComponent if the game has not started.
+ * It renders the TvComponent and DeskComponent if the game has started and the user is not answering questions.
+ * It renders the QuestionAnswerComponent if the game has started and the user is answering questions.
+ * 
+ * @author Zach Sanchez (Zachs00)
+ * @author Ethan Moore (handkrchief)
+ * @version November 21st, 2024
+*/
 
 export default function GameComponent() {
     const {myMaze,isAnsweringQuestions,  started} = useMazeContext();
@@ -13,12 +23,16 @@ export default function GameComponent() {
 
   return (      
     <div className={`background ${s.container}`}>
-        {!started && <div className={s.preGameOptionsContainer}><PreGameOptionsComponent /></div>}
+        {!started && <div className={s.componentContainer}><PreGameOptionsComponent /></div>}
 
-        {started && myMaze && !isAnsweringQuestions && <TvComponent />}
-        {started && myMaze && !isAnsweringQuestions && <DeskComponent/>}
-        {started && !isAnsweringQuestions && <NavigationComponent />}
-        {isAnsweringQuestions && <QuestionAnswerComponent/>}
+        {started && 
+        <>
+
+        {myMaze && !isAnsweringQuestions && <div className={s.componentContainer}><TvComponent /></div>}
+        { myMaze && !isAnsweringQuestions && <div className={s.componentContainer}><DeskComponent/></div>}
+        {!isAnsweringQuestions && <div className={s.componentContainer}><NavigationComponent /></div>}
+        {isAnsweringQuestions && <div className={s.componentContainer}><QuestionAnswerComponent/></div>}
+        </>}
     </div>
   )
 }
